2f3bdd15816e228eab2a0f0c8c71611f0cef046e
Pedro Casas Martínez <pedrocasas876@gmail.com>
2020-11-04 10:50:22 +0100

    Revert "cosas"
    This reverts commit 368c643ce5646faf92b4a44eeacb5bb5692c24a6.

--- neluBackend/src/main/resources/static/scripts/graphics/graphics.js
+++ neluBackend/src/main/resources/static/scripts/graphics/graphics.js
@@ -4,7 +4,6 @@ const tileSize = 32.0;
 class Graphics {
   constructor() {
     this.inputNameField = document.getElementById("userName");
-    this.message = document.getElementById("message");
 
     this.programs = new Map();
     this.parallax = new Parallax();
@@ -171,7 +170,6 @@ class Graphics {
     this.CreateParallaxLayers();
   }
 
-
   AdjustInputFieldPosition(){
     let xSize = 50;
     let fontSize = 50;
@@ -179,17 +177,15 @@ class Graphics {
     this.inputNameField.style.fontSize =""+ (fontSize * Math.min((this.windowRes.x/this.windowRes.y),1))+"px";
     this.inputNameField.style.width = xSize+ "%";
     this.inputNameField.style.left =""+ 50-(xSize*0.5)+"%";
+
     this.inputNameField.style.top = ""+50*Math.min((this.windowRes.x/this.windowRes.y),1)+"%";
 
-    this.message.style.fontSize =""+ (fontSize * Math.min((this.windowRes.x/this.windowRes.y),1))+"px";
-    this.message.style.width = xSize+ "%";
-    this.message.style.left =""+ 50-(xSize*0.5)+"%";
-    this.message.style.top = ""+40*Math.min((this.windowRes.x/this.windowRes.y),1)+"%";
-
+    //Log(this.inputNameField);
   }
 
   CanvasResponsive(forced = false) {
     let newWindowRes = new Vec2(window.innerWidth, window.innerHeight);
+
     this.AdjustInputFieldPosition();
 
     if (!newWindowRes.Equals(this.windowRes) || forced) {
