e41463a431ea96995fceeb50f59ca0b7736ee867
alejandracc <alejandracasado97@gmail.com>
2020-11-10 20:34:28 +0100

    configuracion mas usable?

--- neluBackend/src/main/resources/static/scripts/prefabs/scenesPrefabs/optionsPrefabs.js
+++ neluBackend/src/main/resources/static/scripts/prefabs/scenesPrefabs/optionsPrefabs.js
@@ -11,6 +11,7 @@ prefabFactory.AddPrototype("MoreGraphics", new Vec2(2,2), new Vec2(0.5,0.5), fal
         config = manager.graphics.maxConfig;
       }
       manager.graphics.SetSettingsByNumber(config);
+      ChangeQualityText(config);
     }),
     new TextBox(null, "+","+", new Vec2(0.3,0.1), true),
   ]
@@ -29,6 +30,7 @@ prefabFactory.AddPrototype("LessGraphics", new Vec2(2,2), new Vec2(0.5,0.5), fal
         config=0;
       }
       manager.graphics.SetSettingsByNumber(config);
+      ChangeQualityText(config);
     }),
     new TextBox(null, "-","-", new Vec2(0.3,0.1), true),
   ]
@@ -36,7 +38,7 @@ prefabFactory.AddPrototype("LessGraphics", new Vec2(2,2), new Vec2(0.5,0.5), fal
 prefabFactory.AddPrototype("GraphicsText", new Vec2(8,2), new Vec2(0.5,0.5), false, ()=>{
   return [
     new ImageRenderer(new Vec2(7,4), new Vec2(8,2)),
-    new TextBox("QualityTextOptions", "Calidad", "Quality", new Vec2(0.3,0.1), true),
+    new TextBox("QualityTextOptions", "Calidad: máxima", "Quality: maximum", new Vec2(0.5,0.1), true),
   ]
 });
 prefabFactory.AddPrototype("MenuFromOptions", new Vec2(4,2), new Vec2(0.5,0.0), false, ()=>{
@@ -62,9 +64,11 @@ prefabFactory.AddPrototype("MoreVolume", new Vec2(2,2), new Vec2(0.5,0.5), false
       obj.gameobj.renderer.SetTint(tint[0],tint[1],tint[2]);
     }).SetUpFunc(()=>{
       manager.musicVolume+=0.2;
+      manager.musicVolume=parseFloat(manager.musicVolume.toFixed(1));
       if(manager.musicVolume > 1.0){
         manager.musicVolume = 1.0;
       }
+      ChangeVolumeText();
     }),
     new TextBox(null, "+","+", new Vec2(0.3,0.1), true),
   ]
@@ -79,9 +83,11 @@ prefabFactory.AddPrototype("LessVolume", new Vec2(2,2), new Vec2(0.5,0.5), false
       obj.gameobj.renderer.SetTint(tint[0],tint[1],tint[2]);
     }).SetUpFunc(()=>{
       manager.musicVolume-=0.2;
+      manager.musicVolume=parseFloat(manager.musicVolume.toFixed(1));
       if(manager.musicVolume < 0){
         manager.musicVolume=0;
       }
+      ChangeVolumeText();
     }),
     new TextBox(null, "-","-", new Vec2(0.3,0.1), true),
   ]
@@ -89,7 +95,7 @@ prefabFactory.AddPrototype("LessVolume", new Vec2(2,2), new Vec2(0.5,0.5), false
 prefabFactory.AddPrototype("VolumeText", new Vec2(8,2), new Vec2(0.5,0.5), false, ()=>{
   return [
     new ImageRenderer(new Vec2(7,4), new Vec2(8,2)),
-    new TextBox("VolumeTextOptions", "Volumen","Volume", new Vec2(0.3,0.1), true),
+    new TextBox("VolumeTextOptions", "Volumen: "+manager.musicVolume,"Volume: "+ manager.musicVolume, new Vec2(0.3,0.1), true),
   ]
 });
 prefabFactory.AddPrototype("Sound", new Vec2(2,2), new Vec2(0.5,0.5), false, ()=>{
@@ -141,4 +147,28 @@ function ChangeDificulty(){
 function ChangeLanguage(id,textEn,textSp){
   let text=document.getElementById(id);
   text.innerHTML=manager.english? textEn: textSp;
+}
+function ChangeVolumeText(){
+  var text=document.getElementById("VolumeTextOptions");
+  if(manager.english) text.innerHTML="Volume: "+manager.musicVolume;
+  else text.innerHTML="Volumen: "+manager.musicVolume;
+}
+function ChangeQualityText(config){
+  switch(config){
+    case 0: QualityMessage("minimum","mínima");
+    break;
+    case 1: QualityMessage("low","baja");
+    break;
+    case 2: QualityMessage("medium","mediana");
+    break;
+    case 3: QualityMessage("high","alta");
+    break;
+    case 4: QualityMessage("maximum","máxima");
+    break;
+  }
+}
+function QualityMessage(msgEn,msgSp){
+  var text=document.getElementById("QualityTextOptions");
+  if(manager.english) text.innerHTML="Quality: "+msgEn;
+  else text.innerHTML="Calidad: "+msgSp;
 }
